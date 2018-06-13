386b4f8e4f9a019c859f163976bb670afebdfa98
lj19940623 <lj19940623@icloud.com>
2018-06-13 16:35:05 -0600

    2nd

--- sqlSetup.sql
+++ sqlSetup.sql
@@ -4,33 +4,97 @@ CREATE DATABASE groupen;
 USE groupen;
 CREATE TABLE user
 (
-uid varchar(10),
-psw varchar(10) NOT NULL,
-email varchar(20),
-address varchar(50),
-balance varchar(10),
-PRIMARY KEY (uid)
+    uid varchar(10),
+    psw varchar(10) NOT NULL,
+    email varchar(20),
+    address varchar(50),
+    balance varchar(10),
+    PRIMARY KEY (uid)
 );
+INSERT INTO `user`(`uid`, `psw`, `email`, `address`, `balance`)
+VALUES ("jian","123","jian.li1@uclagary.ca","address1",100);
 CREATE TABLE product
 (
-pid int,
-user_uid varchar(10) NOT NULL,
-price float NOT NULL,
-description varchar(100) NOT NULL,
-tag varchar(10),
-category varchar(10) NOT NULL,
-photo_url varchar(30),
-start_time datetime NOT NULL,
-end_time datetime NOT NULL,
-PRIMARY KEY (pid),
-FOREIGN KEY (user_uid) REFERENCES user (uid)
+    pid int NOT NULL AUTO_INCREMENT,
+    user_uid varchar(10) NOT NULL,
+    price float NOT NULL,
+    description varchar(100) NOT NULL,
+    tag varchar(10),
+    category varchar(10),
+    photo_url varchar(30),
+    start_time datetime NOT NULL,
+    end_time datetime NOT NULL,
+    PRIMARY KEY (pid),
+    FOREIGN KEY (user_uid) REFERENCES user (uid)
 );
-CREATE TABLE group
+CREATE TABLE groups
 (
-gid int,
-starter_uid varchar(10) NOT NULL,
-product_pid int NOT NULL,
-PRIMARY KEY (gid),
-FOREIGN KEY (starter_uid) REFERENCES user (uid),
-FOREIGN KEY (product_pid) REFERENCES product (pid)
+    gid int NOT NULL AUTO_INCREMENT,
+    starter_uid varchar(10) NOT NULL,
+    product_pid int NOT NULL,
+    PRIMARY KEY (gid),
+    FOREIGN KEY (starter_uid) REFERENCES user (uid),
+    FOREIGN KEY (product_pid) REFERENCES product (pid)
+);
+CREATE TABLE groupmember
+(
+    groups_gid int,
+    user_uid varchar(10),
+    PRIMARY KEY (groups_gid, user_uid),
+    FOREIGN KEY (groups_gid) REFERENCES groups (gid),
+    FOREIGN KEY (user_uid) REFERENCES user (uid)
+);
+CREATE TABLE orders
+(
+    oid int AUTO_INCREMENT,
+    user_uid varchar(10) NOT NULL,
+    product_pid int NOT NULL,
+    status varchar(5) NOT NULL,
+    PRIMARY KEY (oid),
+    FOREIGN KEY (user_uid) REFERENCES user (uid),
+    FOREIGN KEY (product_pid) REFERENCES product (pid)
+);
+CREATE TABLE circle
+(
+    cid int AUTO_INCREMENT,
+    name varchar(20) NOT NULL,
+    tag varchar(10),
+    PRIMARY KEY (cid)
+);
+CREATE TABLE circle_user
+(
+    user_uid varchar(10),
+    circle_cid int,
+    PRIMARY KEY (user_uid, circle_cid),
+    FOREIGN KEY (user_uid) REFERENCES user (uid),
+    FOREIGN KEY (circle_cid) REFERENCES circle (cid)
+);
+CREATE TABLE user_msg
+(
+    umid int AUTO_INCREMENT,
+    sender_uid varchar(10) NOT NULL,
+    receiver_uid varchar(10) NOT NULL,
+    context varchar(200) NOT NULL,
+    msg_time datetime DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (umid),
+    FOREIGN KEY (sender_uid) REFERENCES user (uid),
+    FOREIGN KEY (receiver_uid) REFERENCES user (uid)
+);
+CREATE TABLE circle_msg
+(
+    cmid int AUTO_INCREMENT,
+    sender_uid varchar(10) NOT NULL,
+    circle_cid int NOT NULL,
+    context varchar(200) NOT NULL,
+    PRIMARY KEY (cmid),
+    FOREIGN KEY (sender_uid) REFERENCES user (uid),
+    FOREIGN KEY (circle_cid) REFERENCES circle (cid)
+);
+CREATE TABLE friend_with
+(
+    user_uid1 varchar(10),
+    user_uid2 varchar(10),
+    PRIMARY KEY (user_uid1, user_uid2),
+    FOREIGN KEY (user_uid1) REFERENCES user (uid),
+    FOREIGN KEY (user_uid2) REFERENCES user (uid)
 );
