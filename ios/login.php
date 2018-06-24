21243724f6ba220cb10cd18beff1f152030825fc
ZhengLiang95 <zhengliangnnc@gmail.com>
2018-06-23 22:49:04 -0600

    change in ios

--- ios/login.php
+++ ios/login.php
@@ -6,7 +6,7 @@
       $postUsername = $_POST['username'];
       $postPassword = $_POST['password'];
       $link = groupenDB::getInstance();
-      $count = $link -> testIOS($postUsername, $postPassword);
+      $count = $link -> IOSLogin($postUsername, $postPassword);
       if($count!=1){
           $response['message'] = false;
       }else{
